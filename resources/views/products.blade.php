f4eff12dc0753cfb71ddc573403640efd59058cc
Rohit-Takale <rohittakale01@gmail.com>
2024-07-30 14:05:40 +0530

    changes in product page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -28,10 +28,169 @@
  
             </div> -->
 
-            <div class="row portfolio-container " style="">
+            <!-- <div class="row portfolio-container " style="">
+            </div> -->
+
+
+            <div class="row portfolio-container " style="position: relative; height: 0px;">
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/1 HPC.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/1 HPC.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/2 HPCT.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/2 HPCT.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/3 HFS.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/3 HFS.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/4 HLS.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/4 HLS.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/5 HPS.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/5 HPS.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/6 HDP.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/6 HDP.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/7 HDT.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/7 HDT.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/8 HHSP.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/8 HHSP.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/9 HHST.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/9 HHST.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/10 HCHS.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/10 HCHS.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/11 HCHD.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/11 HCHD.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item hydraulics" style="height: 400px; position: absolute; left: 0px; top: 0px;">
+                    <div class="portfolio-wrap">
+                        <img src="https://hydrosquare.in/img/products/hydraulic/12 HTS.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4></h4>
+                            <p></p>
+                            <div class="portfolio-links">
+                                <a href="https://hydrosquare.in/img/products/jacks_detailed/12 HTS.jpg" data-gall="portfolioGallery" class="venobox vbox-item" title=""><i class="bx bx-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            
 
 
 
@@ -42,7 +201,7 @@
     </section>
 
 
-    <script>
+    <!-- <script>
         $(document).ready(function() {
             let list = [{
                     src: "{{asset('img/products/hydraulic/1 HPC.jpg')}}",
@@ -132,6 +291,6 @@
                 </div>`);
             });
         });
-    </script>
+    </script> -->
 
 </x-app-layout>
